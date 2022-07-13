c502162c57868f7a616939e4eead3ea757b06cf6
Youssef Dhraief <dhraief.youssef@gmail.com>
2022-07-13 19:00:40 +0100

    fix: fixed issue with vulnerabilities scan and reporting

--- .custon-ci-actions/trivy-scan-report-parser/script.sh
+++ .custon-ci-actions/trivy-scan-report-parser/script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -n "${PACKAGE_JSON_PATH}" ]; then
   direct_dependencies="$(jq '[(.devDependencies|keys),(.dependencies|keys)]|flatten' "${PACKAGE_JSON_PATH}")"
@@ -8,25 +8,26 @@ output_direct=""
 output_indirect=""
 
 while read -r entry; do
+  if [[ "${entry}" != "null" ]]; then
+    severity="$(echo "${entry}" | jq -r '.Severity')"
+    pkg_name="$(echo "${entry}" | jq -r '.PkgName')"
+    installed_version="$(echo "${entry}" | jq -r '.InstalledVersion')"
+    fixed_version="$(echo "${entry}" | jq -r '.FixedVersion')"
 
-  severity="$(echo "${entry}" | jq -r '.Severity')"
-  pkg_name="$(echo "${entry}" | jq -r '.PkgName')"
-  installed_version="$(echo "${entry}" | jq -r '.InstalledVersion')"
-  fixed_version="$(echo "${entry}" | jq -r '.FixedVersion')"
-
-  if [ -n "${PACKAGE_JSON_PATH}" ]; then
-    if [ "$(echo "${direct_dependencies}" | grep -e "\"${pkg_name}\"" || false)" ]; then
-      output_direct="${output_direct} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
+    if [ -n "${PACKAGE_JSON_PATH}" ]; then
+      if [ "$(echo "${direct_dependencies}" | grep -e "\"${pkg_name}\"" || false)" ]; then
+        output_direct="${output_direct} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
+      else
+        output_indirect="${output_indirect} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
+      fi
     else
-      output_indirect="${output_indirect} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
+      output_direct="${output_direct} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
     fi
-  else
-    output_direct="${output_direct} \n | **${severity}** | ${pkg_name} | \`${installed_version}\` | \`${fixed_version}\` |"
   fi
 
-done < <(jq '.Results[].Vulnerabilities[]' -c "${INPUT_FILE}")
+done < <(jq '.Results[].Vulnerabilities | if . == null then . else .[] end' -c "${INPUT_FILE}")
 
-if [ -n "${PACKAGE_JSON_PATH}" ] && [ "${output_direct}" || "${output_indirect}" ]; then
+if [[ -n "${PACKAGE_JSON_PATH}" && (-n "${output_direct}" || -n "${output_indirect}") ]]; then
   echo "# :warning: These following vulnerabilities were found" >"${OUTPUT_FILE}"
   if [ "${output_direct}" ]; then
     echo -e "## List of **_direct_** dependencies with vulnerabilities \n ${table_header} ${output_direct}" >>"${OUTPUT_FILE}"
@@ -34,7 +35,8 @@ if [ -n "${PACKAGE_JSON_PATH}" ] && [ "${output_direct}" || "${output_indirect}"
   if [ "${output_indirect}" ]; then
     echo -e "## List of **_indirect_** dependencies with vulnerabilities \n ${table_header} ${output_indirect}" >>"${OUTPUT_FILE}"
   fi
-elif [ -z ${PACKAGE_JSON_PATH} ] && [ "${output_direct}" ]; then
+elif [[ -z "${PACKAGE_JSON_PATH}" && -n "${output_direct}" ]]; then
+  echo "# :warning: These following vulnerabilities were found" >"${OUTPUT_FILE}"
   echo -e "## List of dependencies with vulnerabilities \n ${table_header} ${output_direct}" >>"${OUTPUT_FILE}"
 else
   echo "# :white_check_mark: Congratulations! No vulnerabilities were found" >"${OUTPUT_FILE}"
