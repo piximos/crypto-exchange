049ef33029b5f1549243ad5e63daca4fac935c30
Youssef Dhraief <dhraief.youssef@gmail.com>
2022-07-14 08:58:44 +0100

    feat: init helm charts (#3)
    * feat: init helm charts
    
    * fix: fixed issue with charts publisher
    
    * fix: fixed ci issue
    
    * fix: fixed ci issue
    
    * fix: fixed semver issue with chart version
    
    * fix: updated publish code
    
    * fix: updated publish code
    
    * feat: updated helm chart
    
    * feat: added deployment manifest
    
    * ci: updated image tags on build
    
    * build: fixed issue with missing env file

--- .custon-ci-actions/helm-chart-publisher/script.sh
+++ .custon-ci-actions/helm-chart-publisher/script.sh
@@ -17,18 +17,17 @@ exit_with_error_message() {
 
 [ -z "$VERSION_PATH" ] && VERSION_PATH=".version"
 [ -z "$CONSERVE_CURRENT_TAG" ] && current_tag="false"
-[ -z "$CHART_MOUNT_ROOT" ] && exit_with_error_message "'CHART_MOUNT_ROOT' not set. Aborting." 1
 [ -z "$CHART_ROOT" ] && exit_with_error_message "'CHART_ROOT' not set. Aborting." 1
-[ -d "${CHART_MOUNT_ROOT}/$CHART_ROOT" ] || exit_with_error_message "'CHART_ROOT[${CHART_MOUNT_ROOT}/$CHART_ROOT]' does not exist. Aborting." 1
+[ -d "$CHART_ROOT" ] || exit_with_error_message "'CHART_ROOT[$CHART_ROOT]' does not exist. Aborting." 1
 [ -z "$HELM_REGISTRY" ] && exit_with_error_message "'HELM_REGISTRY' not set. Aborting." 1
 [ -z "$HELM_USER" ] && exit_with_error_message "'HELM_USER' not set. Aborting." 1
 [ -z "$HELM_PASSWORD" ] && exit_with_error_message "'HELM_PASSWORD' not set. Aborting." 1
 
-if [[ ! -f "${CHART_MOUNT_ROOT}/${CHART_ROOT}/Chart.yaml" ]]; then
+if [[ ! -f "${CHART_ROOT}/Chart.yaml" ]]; then
   exit_with_error_message "'CHART_ROOT'[$CHART_ROOT] does not contain a valid Helm Chart. Aborting." 2
 fi
 
-current_tag="$(yq eval "${VERSION_PATH}" "${CHART_MOUNT_ROOT}/${CHART_ROOT}/Chart.yaml")"
+current_tag="$(yq eval "${VERSION_PATH}" "${CHART_ROOT}/Chart.yaml")"
 
 if [[ -z $OVERRIDE_TAG && -n $CONSERVE_CURRENT_TAG && $CONSERVE_CURRENT_TAG == "true" ]]; then
   OVERRIDE_TAG="$current_tag"
@@ -68,7 +67,7 @@ generate_new_version() {
   echo "${major_version}.${minor_version}.${patch_version}"
 }
 
-echo "Packaging chart under : ${CHART_MOUNT_ROOT}/${CHART_ROOT}"
+echo "Packaging chart under : ${CHART_ROOT}"
 
 if [[ -n $OVERRIDE_TAG ]]; then
   target_tag="${OVERRIDE_TAG}"
@@ -81,27 +80,29 @@ if [[ -n $TAG_SUFFIX ]]; then
   target_tag="${target_tag}-${TAG_SUFFIX}"
 fi
 
-echo "Bumping tag from $(yq eval "${VERSION_PATH}" "${CHART_MOUNT_ROOT}/${CHART_ROOT}/Chart.yaml") to ${target_tag}"
-yq e "${VERSION_PATH} = \"${target_tag}\"" -i "${CHART_MOUNT_ROOT}/${CHART_ROOT}/Chart.yaml"
+echo "Bumping tag from $(yq eval "${VERSION_PATH}" "${CHART_ROOT}/Chart.yaml") to ${target_tag}"
+yq e "${VERSION_PATH} = \"${target_tag}\"" -i "${CHART_ROOT}/Chart.yaml"
 
 echo "Adding repo '${HELM_REGISTRY}' as library."
 echo "${HELM_PASSWORD}" | helm repo add library "${HELM_REGISTRY}" --username="${HELM_USER}" --password-stdin
 [ $? -eq 0 ] || exit_with_error_message "Unable to login to Chart Museum. Aborting" 4
 
 echo "Installing chart dependencies."
-helm dependency update "${CHART_MOUNT_ROOT}/${CHART_ROOT}/."
+helm dependency update "${CHART_ROOT}/."
 [ $? -eq 0 ] || exit_with_error_message "Unable to download dependencies. Aborting." 5
 
 echo "Linting chart."
-helm lint "${CHART_MOUNT_ROOT}/${CHART_ROOT}/."
+helm lint "${CHART_ROOT}/."
 [ $? -eq 0 ] || exit_with_error_message "Chart linting failed. Aborting." 6
 
 echo "Adding packaging chart."
-helm package "${CHART_MOUNT_ROOT}/${CHART_ROOT}"
+helm package "${CHART_ROOT}"
 [ $? -eq 0 ] || exit_with_error_message "Chart packaging failed. Aborting." 7
 
 echo "Adding pushing chart."
-helm cm-push "${CHART_MOUNT_ROOT}/${CHART_ROOT}" library
+chart_name="$(yq eval ".name" "${CHART_ROOT}/Chart.yaml")"
+chart_tgz="${chart_name}-${target_tag}.tgz"
+curl -u"${HELM_USER}:${HELM_PASSWORD}" -T "${chart_tgz}" "https://piximos.jfrog.io/artifactory/piximos-helm/${chart_name}/${chart_tgz}"
 [ $? -eq 0 ] || exit_with_error_message "Failed to push chart. Aborting." 8
 
 exit 0
